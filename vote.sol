de0d5639fca52eac9fb19b23b036a43cfd410bc1
Josh-Payne <snare117@gmail.com>
2018-02-27 11:09:09 -0800

    re-commit solidity, js

---
+++ vote.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.20
+
+contract Voting {
+
+    mapping (bytes32 ==> uint8) public votesReceived
+
+    bytes32[] public candidateList;
+
+    function Voting(bytes32[] candidateNames) {
+        candidateList = candidateNames
+    }
+
+    function totalVotes(bytes32 candidate) returns (uint8) {
+        return votesReceived[candidate];
+    }
+
+    function voteForCandidate(bytes32 candidate) {
+        if(validCandidate(candidate) == false) throw;
+        votesReceived[candidate]+=1;
+    } 
+
+    function validCandidate(bytes32 candidate) returns (bool) {
+        for(uint i = 0; i < candidateList.length; i++) {
+            if (candidateList[i] == candidate) {
+                return true;
+            }
+        }
+    }
+}
